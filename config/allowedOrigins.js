fcacbc0b7a0f095d3ed525d4f1cc07544ba96eee
Vineet-Singh-Chauhan <chauhansvineet@gmail.com>
2023-06-03 23:13:10 +0530

    added server address in allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
   "http://127.0.0.1:5173",
   "http://localhost:5173",
   "https://expedite-client.vercel.app",
+  "https://expedite-server.vercel.app",
 ];
 
 module.exports = allowedOrigins;
