5b5c767a3937ee5d353c568032e7b51ce66aa173
Vineet-Singh-Chauhan <chauhansvineet@gmail.com>
2023-06-03 23:56:28 +0530

    add smtp host in allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,6 +2,8 @@ const allowedOrigins = [
   "http://127.0.0.1:5173",
   "http://localhost:5173",
   "https://expedite-client.vercel.app",
+  "https://expedite-server.vercel.app",
+  "smtp.office365.com",
 ];
 
 module.exports = allowedOrigins;
